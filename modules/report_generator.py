84dc9121c1591d30b81366994820bd3fd86239d9
Suraj Singh R <suraj3tc@gmail.com>
2026-08-05 19:19:04 +0530

    Update report_generator.py

--- modules/report_generator.py
+++ modules/report_generator.py
@@ -1,13 +1,74 @@
 import pandas as pd
+import json
+import os
+
+
+REPORT_FOLDER = "reports"
+
+
+def create_report_folder():
+
+    if not os.path.exists(REPORT_FOLDER):
+        os.makedirs(REPORT_FOLDER)
 
 
 def generate_csv(data):
 
+    create_report_folder()
+
     df = pd.DataFrame(data)
 
+    csv_path = os.path.join(
+        REPORT_FOLDER,
+        "infrastructure_report.csv"
+    )
+
     df.to_csv(
-        "reports/infrastructure_report.csv",
+        csv_path,
         index=False
     )
 
-    print("\nCSV Report Generated Successfully")
+    print("✓ CSV Report Generated")
+
+
+def generate_excel(data):
+
+    create_report_folder()
+
+    df = pd.DataFrame(data)
+
+    excel_path = os.path.join(
+        REPORT_FOLDER,
+        "infrastructure_report.xlsx"
+    )
+
+    df.to_excel(
+        excel_path,
+        index=False
+    )
+
+    print("✓ Excel Report Generated")
+
+
+def generate_json(data):
+
+    create_report_folder()
+
+    json_path = os.path.join(
+        REPORT_FOLDER,
+        "infrastructure_report.json"
+    )
+
+    with open(
+        json_path,
+        "w"
+    ) as file:
+
+        json.dump(
+            data,
+            file,
+            indent=4,
+            default=str
+        )
+
+    print("✓ JSON Report Generated")
